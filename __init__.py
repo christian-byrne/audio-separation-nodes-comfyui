665b225b332fb50b513353f929e381163dda3069
bymyself <cbyrne@comfy.org>
2025-12-23 17:16:37 -0800

    Support absolute imports when package context missing

--- __init__.py
+++ __init__.py
@@ -1,10 +1,19 @@
-from .src.separation import AudioSeparation
-from .src.tempo_match import TempoMatch
-from .src.crop import AudioCrop
-from .src.combine import AudioCombine
-from .src.combine_video_with_audio import AudioVideoCombine
-from .src.time_shift import TimeShift
-from .src.get_tempo import GetTempo
+try:
+    from .src.separation import AudioSeparation
+    from .src.tempo_match import TempoMatch
+    from .src.crop import AudioCrop
+    from .src.combine import AudioCombine
+    from .src.combine_video_with_audio import AudioVideoCombine
+    from .src.time_shift import TimeShift
+    from .src.get_tempo import GetTempo
+except ImportError:  # pragma: no cover
+    from src.separation import AudioSeparation
+    from src.tempo_match import TempoMatch
+    from src.crop import AudioCrop
+    from src.combine import AudioCombine
+    from src.combine_video_with_audio import AudioVideoCombine
+    from src.time_shift import TimeShift
+    from src.get_tempo import GetTempo
 
 
 NODE_CLASS_MAPPINGS = {
